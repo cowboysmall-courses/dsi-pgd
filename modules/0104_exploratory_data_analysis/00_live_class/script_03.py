3311c7c57fbd29ae35c941cb268b1c5af1ea9cd1
Jerry Kiely <jerry@cowboysmall.com>
2023-10-07 12:30:20 +0100

    added some live class scripts

---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_03.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 10:14:26 2023
+
+@author: jerry
+"""
+
+
+import pandas as pd
+
+
+salary_data = pd.read_csv("../../../data/data_management/basic_salary.csv")
+
+
+sorted_ba = salary_data.sort_values(by = ['ba'])
+sorted_ba.head()
+
+
+sorted_ba = salary_data.sort_values(by = ['ba'], ascending = [0])
+sorted_ba.head()
+
+
+sorted_grade = salary_data.sort_values(by = ['Grade'])
+sorted_grade.head()
+
+
+sorted_grade_ba = salary_data.sort_values(by = ['Grade', 'ba'])
+sorted_grade_ba.head(n = 10)
+
+
+sorted_grade_ba = salary_data.sort_values(by = ['Grade', 'ms'], ascending = [0, 1])
+sorted_grade_ba.head(n = 10)
---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_05.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 10:42:06 2023
+
+@author: jerry
+"""
+
+
+
+import pandas as pd
+
+
+salary1 = pd.read_csv("../../../data/data_management/basic_salary - 1.csv")
+salary2 = pd.read_csv("../../../data/data_management/basic_salary - 2.csv")
+
+frames = [salary1, salary2]
+append1 = pd.concat(frames)
+append1
+
+append2 = pd.concat([salary1, salary2])
+append2
+
+append3 = pd.concat([salary1, salary2], ignore_index = True)
+append3
