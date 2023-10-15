1f4bb911aea9db2e8a035ab913fed7fce48c11cf
Jerry Kiely <jerry@cowboysmall.com>
2023-10-15 09:37:55 +0100

    added some data, scripts, and notes

---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_04.R
@@ -0,0 +1,73 @@
+
+library(ggplot2)
+
+
+
+# 1
+demo_data <- read.csv("../../../data/live_class/TelecomData_CustDemo.csv", header = TRUE)
+weekly_data <- read.csv("../../../data/live_class/TelecomData_WeeklyData.csv", header = TRUE)
+
+
+head(demo_data)
+head(weekly_data)
+
+
+tx_data <- aggregate(Calls ~ CustID, data = weekly_data, FUN = sum)
+head(tx_data)
+
+
+work_data <- merge(demo_data, tx_data, by = ('CustID'), all = TRUE)
+head(work_data)
+
+
+work_data$age_group <- cut(work_data$Age, breaks = c(0, 30, 45, Inf), labels = c("18-30", "30-45", ">45"))
+head(work_data)
+
+
+ggplot(work_data, aes(x = age_group)) + geom_bar()
+
+
+
+ggplot(work_data, aes(x = age_group, y = Calls)) + 
+  geom_bar(stat = "identity", fill = "orange") + 
+  labs(x = "Age Groups", y = "Total Calls", title = "Bar Chart") +
+  coord_flip()
+
+
+ggplot(work_data, aes(x = age_group)) + 
+  geom_bar(aes(fill = Gender)) + 
+  labs(x = "Gender", y = "No. of Customers", title = "Stacked Bar Chart")
+
+
+ggplot(work_data, aes(x = age_group)) + 
+  geom_bar(aes(fill = Gender), position = "dodge") + 
+  labs(x = "Gender", y = "No. of Customers", title = "Multiple Bar Chart")
+
+
+ggplot(work_data, aes(x = "", y = Calls)) + 
+  geom_boxplot() + 
+  labs(y = "Total Calls", title = "Box Plot")
+
+
+ggplot(work_data, aes(x = age_group, y = Calls)) + 
+  geom_boxplot() + 
+  labs(x = "Age Group", y = "Total Calls", title = "Box Plot")
+
+
+ggplot(work_data, aes(x = age_group, y = Calls)) + 
+  geom_boxplot(fill = 5, outlier.color = "blue", outlier.size = 2.5) + 
+  labs(x = "Age Group", y = "Total Calls", title = "Box Plot")
+
+
+ggplot(work_data, aes(x = age_group, y = Calls)) + 
+  geom_boxplot(aes(fill = Gender)) + 
+  labs(x = "Age Group", y = "Total Calls", title = "Box Plot")
+
+
+ggplot(work_data, aes(x = Calls)) + 
+  geom_histogram(binwidth = 40, fill = "maroon") + 
+  labs(x = "Total Calls", y = "No. of Customers", title = "Customer Usage")
+
+
+
+
---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_05.R
@@ -0,0 +1,38 @@
+
+library(ggplot2)
+library(GGally)
+
+job_data_1 <- read.csv("../../../data/live_class/JOB\ PROFICIENCY\ DATA.csv", header = TRUE)
+head(job_data_1)
+
+
+ggplot(job_data_1, aes(x = aptitude, y = job_prof)) + 
+  geom_point() + geom_smooth(method = "lm") + 
+  labs(x = "Aptitude", y = "Job Proficiency", title = "Scatter Plot")
+
+
+
+job_data_2 <- read.csv("../../../data/live_class/JOB\ PROFICIENCY\ DATA\ for\ Bubble\ Chart.csv", header = TRUE)
+head(job_data_2)
+
+
+qplot(
+  aptitude, 
+  tech_, 
+  data = job_data_2, 
+  size = job_prof, 
+  color = function., 
+  xlab = "Aptitude",  
+  ylab = "Technical", 
+  main = "Bubble Chart"
+)
+
+
+pairs(job_data_1)
+
+
+ggpairs(
+  job_data_1[, c("aptitude", "testofen", "tech_", "g_k_", "job_prof")],
+  title = "Scatter Plot"
+)
+
