1cc68e0463355553abbe30271c1998e21d7eeb29
Jerry Kiely <jerry@cowboysmall.com>
2023-10-19 19:50:35 +0100

    added live class script and data

---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_08.R
@@ -0,0 +1,54 @@
+
+house_data <- read.csv("../../../data/eda/live_class/Housing_Prices.csv", header = TRUE)
+
+head(house_data)
+summary(house_data)
+dim(house_data)
+
+
+# CRIM -> Per Capita Crime Rate by town
+# NOX  -> Nitric Oxides concentration
+# MEDV -> Median Value of owner-occupied homes in $1000's
+
+
+library(GGally)
+
+ggpairs(house_data, title = "Scatter Plot Matrix")
+
+cor(house_data$MEDV, house_data$CRIM)
+cor(house_data$MEDV, house_data$NOX)
+
+
+
+# Simple Linear Regression: MEDV vs CRIM
+model1 <- lm(MEDV ~ CRIM, data = house_data)
+summary(model1)
+
+house_data$pred1 <- fitted(model1)
+house_data$resi1 <- residuals(model1)
+head(house_data)
+
+
+
+# Simple Linear Regression: MEDV vs NOX
+model2 <- lm(MEDV ~ NOX, data = house_data)
+summary(model2)
+
+house_data$pred2 <- fitted(model2)
+house_data$resi2 <- residuals(model2)
+head(house_data)
+
+
+
+# Multiple Linear Regression: MEDV vs NOX
+model3 <- lm(MEDV ~ CRIM + NOX, data = house_data)
+summary(model3)
+
+house_data$pred3 <- fitted(model3)
+house_data$resi3 <- residuals(model3)
+head(house_data)
+
+
+
+
+
