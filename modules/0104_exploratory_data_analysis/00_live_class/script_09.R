8ebbfc6550e9f15b20f9d114b5236622f46942d4
Jerry Kiely <jerry@cowboysmall.com>
2023-10-23 19:38:37 +0100

    added live class sample data and script

---
+++ modules/0104_exploratory_data_analysis/00_live_class/script_09.R
@@ -0,0 +1,21 @@
+
+library(e1071)
+
+
+data <- read.csv("../../../data/eda/live_class/Normality_Assessment_Data.csv", header = TRUE)
+
+
+boxplot(data$csi, col = "cadetblue")
+boxplot(data$billamt, col = "cadetblue")
+
+
+skewness(data$csi)
+skewness(data$billamt)
+
+
+qqnorm(data$csi, col = "cadetblue")
+qqnorm(data$billamt, col = "cadetblue")
+
+
+
+
