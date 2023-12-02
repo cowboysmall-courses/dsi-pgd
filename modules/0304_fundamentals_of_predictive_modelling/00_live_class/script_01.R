195a1df86b295e9948a879e607ed3f85424c4d99
Jerry Kiely <jerry@cowboysmall.com>
2023-12-02 15:49:46 +0000

    added some notes + added a new live class script + added some data

--- modules/0304_fundamentals_of_predictive_modelling/00_live_class/script_01.R
+++ modules/0304_fundamentals_of_predictive_modelling/00_live_class/script_01.R
@@ -1,25 +1,100 @@
 
-train_data <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/predictive_modelling/Performance\ Index.csv", header = TRUE)
-head(train_data)
+library(nortest)
+library(car)
 
 
-model <- lm(jpi ~ aptitude + tol + technical + general, data = train_data)
-summary(model)
 
 
-model <- lm(jpi ~ aptitude + technical + general, data = train_data)
-summary(model)
+
+# 
+
+train_data1 <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/predictive_modelling/Performance\ Index.csv", header = TRUE)
+head(train_data1)
 
 
-train_data$pred <- fitted(model)
-train_data$resi <- residuals(model)
-head(train_data)
+model1 <- lm(jpi ~ aptitude + tol + technical + general, data = train_data1)
+summary(model1)
+
+X <- cbind(rep(1, nrow(train_data1)), as.matrix(train_data1[, c("aptitude", "tol", "technical", "general")]))
+Y <- as.matrix(train_data1[, "jpi"])
+B <- solve(t(X) %*% X) %*% t(X) %*% Y
+round(B[, 1], 5)
 
 
-test_data <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/predictive_modelling/Performance\ Index\ new.csv", header = TRUE)
-head(test_data)
+vif(model1)
+
+train_data1$pred <- fitted(model1)
+train_data1$resi <- residuals(model1)
+head(train_data1)
 
 
-test_data$pred <- predict(model, test_data)
-test_data$resi <- test_data$jpi - test_data$pred
-head(test_data)
+
+plot(train_data1$jpi, train_data1$resi, col = "cadetblue")
+
+qqnorm(train_data1$resi, col = "cadetblue")
+qqline(train_data1$resi, col = "cadetblue", lwd = 2)
+
+shapiro.test(train_data1$resi)
+lillie.test(train_data1$resi)
+
+
+
+
+
+# 
+
+train_data2 <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/predictive_modelling/Performance\ Index.csv", header = TRUE)
+head(train_data2)
+
+model2 <- lm(jpi ~ aptitude + technical + general, data = train_data2)
+summary(model2)
+
+vif(model2)
+
+train_data2$pred <- fitted(model2)
+train_data2$resi <- residuals(model2)
+head(train_data2)
+
+
+
+plot(train_data2$jpi, train_data2$resi, col = "cadetblue")
+
+qqnorm(train_data2$resi, col = "cadetblue")
+qqline(train_data2$resi, col = "cadetblue", lwd = 2)
+
+shapiro.test(train_data2$resi)
+lillie.test(train_data2$resi)
+
+
+
+
+
+# 
+
+test_data1 <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/predictive_modelling/Performance\ Index\ new.csv", header = TRUE)
+head(test_data1)
+
+
+test_data1$pred <- predict(model, test_data1)
+test_data1$resi <- test_data1$jpi - test_data1$pred
+head(test_data1)
+
+
+
+plot(test_data1$jpi, test_data1$resi, col = "cadetblue")
+
+qqnorm(test_data1$resi, col = "cadetblue")
+qqline(test_data1$resi, col = "cadetblue", lwd = 2)
+
+shapiro.test(test_data1$resi)
+lillie.test(test_data1$resi)
+
+
+
+
+
+# 
+
+
+
+
---
+++ modules/0304_fundamentals_of_predictive_modelling/00_live_class/script_02.R
@@ -0,0 +1,72 @@
+
+library(nortest)
+library(car)
+library(GGally)
+
+
+
+data <- read.csv("../../../data/0304_fundamentals_of_predictive_modelling/live_class/car\ price\ data.csv", header = TRUE)
+head(data)
+
+ggpairs(
+  data[, c("RESALE.PRICE", "ENGINE.SIZE", "HORSE.POWER", "WEIGHT", "YEARS")],
+  title = "Scatter Plot Matrix",
+  columnLabels = c("Resale Price", "Engine Sie", "Horse Power", "Weight", "Years")
+)
+
+
+
+
+
+# 
+
+model <- lm(RESALE.PRICE ~ ENGINE.SIZE + HORSE.POWER + WEIGHT + YEARS, data = data)
+summary(model)
+
+vif(model)
+
+
+
+
+
+# 
+
+model <- lm(RESALE.PRICE ~ HORSE.POWER + WEIGHT + YEARS, data = data)
+summary(model)
+
+vif(model)
+
+
+
+
+
+# 
+
+model <- lm(RESALE.PRICE ~ HORSE.POWER + WEIGHT, data = data)
+summary(model)
+
+vif(model)
+
+
+
+
+
+
+# 
+
+data$pred <- fitted(model)
+data$resi <- residuals(model)
+head(data)
+
+
+
+plot(data$RESALE.PRICE, data$resi, col = "cadetblue")
+
+qqnorm(data$resi, col = "cadetblue")
+qqline(data$resi, col = "cadetblue", lwd = 2)
+
+shapiro.test(data$resi)
+lillie.test(data$resi)
+
+
+
