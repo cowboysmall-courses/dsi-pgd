7a7f52ab5bf98d60722ee1fc6b5158e20fe76e20
Jerry Kiely <jerry@cowboysmall.com>
2024-05-11 13:35:07 +0100

    added assignment scripts

---
+++ scripts/0704_machine_learning_01/04_assignment/assignment_01.py
@@ -0,0 +1,29 @@
+
+
+# BACKGROUND:
+#
+#     The data is a marketing campaign data of a skin care clinic associated with its success.
+#
+#     Description of variables:
+#
+#         Success:         Response to marketing campaign of Skin Care Clinic which offers both products and services. (1: email Opened, 0: email not opened)
+#         AGE:             Age Group of Customer
+#         Recency_Service: Number of days since last service purchase
+#         Recency_Product: Number of days since last product purchase
+#         Bill_Service:    Total bill amount for service in last 3 months
+#         Bill_Product:    Total bill amount for products in last 3 months
+#         Gender:          (1: Male, 2: Female)
+#
+#     Note: Answer following questions using entire data and do not create test data.
+#
+# QUESTIONS
+#
+#     1: Import Email Campaign data. Perform binary logistic regression to model “Success”. Interpret sign of each significant variable in the model.
+#     2: Compare performance of Binary Logistic Regression (significant variables) and Naïve Bayes Method (all variables) using area under the ROC curve.
+#     3: Implement binary logistic regression and Support Vector Machines by combining service and product variables. State area under the ROC curve in each case.
+
+
+
+
+
+
