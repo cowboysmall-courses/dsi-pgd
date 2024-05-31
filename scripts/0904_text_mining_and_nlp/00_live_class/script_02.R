c13b6ab5d3b8ebce0432bbc6c803a246c70302a8
Jerry Kiely <jerry@cowboysmall.com>
2024-05-31 08:40:37 +0100

    re-arranged some live class scripts

--- scripts/0904_text_mining_and_nlp/00_live_class/script_02.R
+++ scripts/0904_text_mining_and_nlp/00_live_class/script_02.R
@@ -1,125 +1,10 @@
 
-library(tm)
-library(wordcloud)
-library(ggplot2)
 library(sentimentr)
 library(syuzhet)
 
 
 data <- readLines("./data/0904_text_mining_and_nlp/00_live_class/HR Appraisal process.txt")
-head(data)
-# [1] "The process was transparent."                                                                    "There is a lot of scope to improve the process, as most questions were subjective."             
-# [3] "Happy with the process, but salary increment in 2019 is very low as compared to previous years." "Many questions were very subjective. Very difficult to measure the performance."                
-# [5] "Questions could have been specific to function. Very general questions."                         "More research is required to come out with better process next time."  
-
-
-corp <- Corpus(VectorSource(data))
-class(corp)
-# "SimpleCorpus" "Corpus"
-
-
-inspect(corp[1:3])
-# <<SimpleCorpus>>
-# Metadata:  corpus specific: 1, document level (indexed): 0
-# Content:  documents: 3
-
-# [1] The process was transparent.                                                                    There is a lot of scope to improve the process, as most questions were subjective.             
-# [3] Happy with the process, but salary increment in 2019 is very low as compared to previous years.
-
-
-writeLines(as.character(corp[[3]]))
-# Happy with the process, but salary increment in 2019 is very low as compared to previous years.
-
-
-corp <- tm_map(corp, tolower)
-writeLines(as.character(corp[[3]]))
-# happy with the process, but salary increment in 2019 is very low as compared to previous years.
-
-
-corp <- tm_map(corp, removePunctuation)
-writeLines(as.character(corp[[3]]))
-# happy with the process but salary increment in 2019 is very low as compared to previous years
-
-
-corp <- tm_map(corp, removeNumbers)
-writeLines(as.character(corp[[3]]))
-# happy with the process but salary increment in  is very low as compared to previous years
-
-
-corp <- tm_map(corp, removeWords, stopwords("english"))
-writeLines(as.character(corp[[3]]))
-# happy   process  salary increment     low  compared  previous years
-
-
-corp <- tm_map(corp, removeWords, "process")
-writeLines(as.character(corp[[3]]))
-# happy     salary increment     low  compared  previous years
-
-
-tdm <- TermDocumentMatrix(corp)
-findFreqTerms(tdm)
-#  [1] "transparent"     "improve"         "lot"             "questions"       "scope"           "subjective"      "compared"        "happy"           "increment"       "low"             "previous"        "salary"          "years"          
-# [14] "difficult"       "many"            "measure"         "performance"     "function"        "general"         "specific"        "better"          "come"            "next"            "required"        "research"        "time"           
-# [27] "adopted"         "fair"            "benchmark"       "extremely"       "industry"        "methodology"     "rating"          "effort"          "excellent"       "team"            "congratulations" "department"      "improvement"    
-# [40] "needs"           "approach"        "current"         "discussion"      "frequent"        "manager"         "using"           "evaluate"        "possible"        "work"            "disappointed"    "little"          "biased"         
-# [53] "need"            "expected"        "method"          "used"            "good"            "changes"         "clear"           "twice"           "year"            "can"             "consultant"      "hire"            "clearer"        
-# [66] "last"            "selfassessment"  "particular"      "toward"          "appraisal"       "think"           "carried"         "organization"    "way"             "modified"        "communication"   "overall"         "satisfied"      
-# [79] "remains"         "keep"            "members"         "show"            "make"            "minor"           "robust"          "will"            "removed"         "replaced"        "headvery"        "nice"            "smooth"         
-# [92] "appreciate"      "processmust"    
-
-
-findFreqTerms(tdm, 5)
-# "questions"   "subjective"  "happy"       "difficult"   "measure"     "performance" "fair"        "work" 
-
-
-findAssocs(tdm, "difficult", 0.6)
-# $difficult
-#     measure performance    approach       using
-#        1.00        0.90        0.61        0.61
-
-
-findAssocs(tdm, "questions", 0.6)
-# $questions
-# subjective
-#       0.67
-
-
-m <- as.matrix(tdm)
-
-
-v <- sort(rowSums(m), decreasing = TRUE)
-d <- data.frame(word = names(v), freq = v)
-head(d)
-#                    word freq
-# questions     questions   13
-# happy             happy   10
-# subjective   subjective    8
-# fair               fair    7
-# performance performance    6
-# work               work    6
-
-pal2 <- brewer.pal(8, "Dark2")
-
-
-wordcloud(d$word, d$freq, random.order = FALSE, min.freq = 1, colors = pal2)
-
-
-term.freq <- rowSums(m)
-term.freq <- subset(term.freq, term.freq >= 5)
-
-df <- data.frame(term = names(term.freq), freq = term.freq)
-
-ggplot(df, aes(x = term, y = freq)) +
-    geom_bar(stat = "identity") +
-    xlab("Terms") +
-    ylab("Count") +
-    coord_flip()
-
-
-
-
-data2 <- readLines("./data/0904_text_mining_and_nlp/00_live_class/HR Appraisal process.txt")
-sentiment(data2)
+sentiment(data)
 # Key: <element_id, sentence_id>
 #     element_id sentence_id word_count   sentiment
 #          <int>       <int>      <int>       <num>
@@ -194,13 +79,13 @@ sentiment(data2)
 # 69:         48           2          4  0.25000000
 #     element_id sentence_id word_count   sentiment
 
-sentiment_by(data2)
+sentiment_by(data)
 
 
-get_sentiment(data2)
+get_sentiment(data)
 
 
-nrcsentiment <- get_nrc_sentiment(data2)
+nrcsentiment <- get_nrc_sentiment(data)
 head(nrcsentiment)
 #   anger anticipation disgust fear joy sadness surprise trust negative positive
 # 1     0            0       0    0   0       0        0     0        0        0
