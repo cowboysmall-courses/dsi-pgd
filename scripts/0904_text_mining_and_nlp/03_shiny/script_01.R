29db10dc5f48b487b1b4bc8e73362e65032fd01e
Jerry Kiely <jerry@cowboysmall.com>
2024-06-25 00:07:38 +0100

    some cleanup

--- scripts/0904_text_mining_and_nlp/03_shiny/script_01.R
+++ scripts/0904_text_mining_and_nlp/03_shiny/script_01.R
@@ -4,6 +4,3 @@ library(shiny)
 
 runExample("01_hello")
 runExample("01_hello", display.mode = "showcase")
-
-
-runApp("./scripts/0904_text_mining_and_nlp/03_shiny/shinyapp")
---
+++ scripts/0904_text_mining_and_nlp/03_shiny/script_02.R
@@ -0,0 +1,5 @@
+
+library(shiny)
+
+
+runApp("./scripts/0904_text_mining_and_nlp/03_shiny/shinyapp")
